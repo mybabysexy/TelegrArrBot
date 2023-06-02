1b6fe0523fca28996e6c79e335545aff61a0f9f2
duc1607 <3dogize@gmail.com>
2023-06-02 14:59:39 +0700

    Updated Help

--- handler/help.handler.ts
+++ handler/help.handler.ts
@@ -5,17 +5,17 @@ class HelpHandler {
         const COMMANDS = [
             `<code>/meow - Meow 😺</code>`,
             `<code>/bau - Meow 😺</code>`,
-
+            '',
             `<code>kho phim - View Movies Library</code>`,
             `<code>/movies - View Movies Library</code>`,
-
+            '',
             `<code>phim [keyword] - Lookup movies</code>`,
             `<code>/movie [keyword] - Lookup movies</code>`,
             `<code>/imdb [id] - Lookup movie by IMDB ID</code>`,
             `<code>/tmdb [id] - Lookup movie by TMDB ID</code>`,
-
+            '',
             `<code>/queue - View queue status</code>`,
-
+            '',
             `<code>/help - Show this help duh</code>`,
         ];
         return ctx.replyWithHTML(`<b>Available commands:</b>\n${COMMANDS.join("\n")}`);
